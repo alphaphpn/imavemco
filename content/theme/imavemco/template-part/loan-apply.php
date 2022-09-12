cd43b13649e7c6cf44cb07b6ef6f46d3f549ff06
AlphaPhpN <alphaphpn@gmail.com>
2022-09-12 13:43:31 +0800

    Update loan-apply.php
    adjust padding

--- content/theme/imavemco/template-part/loan-apply.php
+++ content/theme/imavemco/template-part/loan-apply.php
@@ -4,11 +4,11 @@
 				<div class="col-sm-6 left-img-wide-bg-cover" style="background-image: url('<?php echo $domainhome.'content/theme/'.$themename.'/storage/img/loan/online_loan_application.jpg'; ?>');"></div>
 				<div class="col-sm-6 right-content-wide">
 					<div class="card mx-auto" style="max-width: 568px;">
-						<div class="card-header bg-primary text-white p-5">
+						<div class="card-header bg-primary text-white p-3">
 							<h2 class="text-center">How much you want?</h1>
 							<p>We offer online rapid cash loans to borrowers in other countries, as well as cash loans in person to borrowers in the local.</p>
 						</div>
-						<div class="card-body p-5">
+						<div class="card-body p-3">
 							<form id="apply-loan" method="post" class="needs-validation" novalidate>
 								<div class="form-group">
 									<label for="amount-want">Amount you want</label>
@@ -37,7 +37,7 @@
 								</div>
 							
 						</div>
-						<div class="card-footer bg-primary text-white p-5">
+						<div class="card-footer bg-primary text-white p-3">
 							<button class="btn btn-danger btn-lg w-100">Sign Up</button>
 							</form>
 						</div>
